a3c0be22ce2915177398a58f0881b5484c7fa811
Juxi <juxi.leitner@gmail.com>
2013-12-05 23:59:25 +0100

    adding an iCub logo

--- icVision-AndroidTest/gen/net/Juxi/icvision/test/R.java
+++ icVision-AndroidTest/gen/net/Juxi/icvision/test/R.java
@@ -25,14 +25,16 @@ or to a theme attribute in the form "<code>?[<i>package</i>:][<i>type</i>:]<i>na
     public static final class drawable {
         public static final int ic_1_navigation_refresh=0x7f020000;
         public static final int ic_2_action_settings=0x7f020001;
-        public static final int ic_launcher=0x7f020002;
+        public static final int ic_icub=0x7f020002;
+        public static final int ic_launcher=0x7f020003;
     }
     public static final class id {
         public static final int ImageButtonSettings=0x7f090004;
-        public static final int action_refresh=0x7f090006;
-        public static final int action_settings=0x7f090007;
+        public static final int action_refresh=0x7f090007;
+        public static final int action_settings=0x7f090008;
         public static final int fullscreen_content=0x7f090003;
-        public static final int module_detail=0x7f090005;
+        public static final int imageButton1=0x7f090005;
+        public static final int module_detail=0x7f090006;
         public static final int module_detail_container=0x7f090000;
         public static final int module_list=0x7f090001;
         public static final int txtLog=0x7f090002;
